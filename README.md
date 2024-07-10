5c6808b5a273085bcea6742826a6fe7c4e72bc99
klue <klue__@naver.com>
2024-07-11 02:24:29 +0900

    set Ubuntu 22.04

--- README.md
+++ README.md
@@ -1,11 +1,36 @@
 # Vim-Setting
+
+## OS, CPU & GPU configuration
+### Ubuntu version
+first check your Ubuntu version, choose one of them
+```bash
+cat /etc/issue
+lsb_release -a
+cat /etc/os-release
+```
+
+### CPU configuration
+```bash
+# version
+lscpu
+```
+
 ## How to set vim
+### If you use, 22.04 Ubuntu or upper version,
 ```bash
 bash install.sh
 ```
-after that
+
+### If you use, 20.04 Ubuntu or under version,
+```bash
+bash install-o.sh
+```
+and next
 ```bash
 bash setting.sh
 ```
+
+
+
 ## License
 fork from https://github.com/heelim/setting
--- install.sh
+++ install.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-sudo apt-get install -y build-essential libncurses-dev clang-format-10 zsh curl libpython3
+sudo apt-get install -y build-essential libncurses-dev clang-format zsh curl libpython3-dev
 
 # tmux minimal
 bash -c "$(curl -s https://raw.githubusercontent.com/umayr/tmux/master/init.sh)"
 
 # oh-my-zsh
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp ~/setting/.vimrc ~/.vimrc
+
+# Powerlevel 10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+#autojump
+git clone https://github.com/wting/autojump.git
+cd autojump
+python3 ./install.py
+
+#zsh syntax-highlight
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+#zsh auto-suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#cp configs
+cd ../autojump
+cp .zshrc ~/.zshrc
+cp .p10k.zsh ~/.p10k.zsh
+cp .tmux.conf ~/.tmux.conf
